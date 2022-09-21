47d8054790823aed31ad130d2d1263c54f4a94ed
Zoroark <utybodev@gmail.com>
2022-09-21 19:39:12 +0200

    Add packages, edge wayland setup and better hidpi

--- helpers/desktop.nix
+++ helpers/desktop.nix
@@ -4,7 +4,8 @@ let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
   imports = [
-    ../helpers/hidpi.nix
+    ./hidpi.nix
+    ./shell.nix
   ];
 
   home.packages = [
@@ -13,6 +14,39 @@ in {
     pkgs.gnome.gnome-tweaks
     pkgs.slack
     pkgs.gh
+    pkgs.jdk
+    pkgs.jdk17
+    pkgs.maven
+    pkgs.wl-clipboard
+    unstable.jetbrains.idea-ultimate
+    (
+      let
+        microsoft-edge-no-desktop = pkgs.microsoft-edge.overrideAttrs (oldAttrs: rec {
+          fixupPhase = ''
+            mv $out/share/applications/microsoft-edge.desktop $out/share/applications/microsoft-edge.desktop.disabled
+
+            substituteInPlace $out/bin/microsoft-edge \
+              --replace /bin/bash ${pkgs.bash}/bin/bash
+          '';
+        });
+      in pkgs.symlinkJoin {
+        name = "zoroedge";
+        paths = [ microsoft-edge-no-desktop ];
+        buildInputs = [ microsoft-edge-no-desktop pkgs.makeWrapper pkgs.wayland ];
+        postBuild = ''
+          cp ${microsoft-edge-no-desktop}/share/applications/microsoft-edge.desktop.disabled \
+            $out/share/applications/microsoft-edge.desktop
+          
+          substituteInPlace $out/share/applications/microsoft-edge.desktop \
+            --replace ${microsoft-edge-no-desktop}/bin/microsoft-edge $out/bin/microsoft-edge
+          
+          wrapProgram $out/bin/microsoft-edge \
+            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland" \
+            --set LD_LIBRARY_PATH ${pkgs.wayland}/lib
+        '';
+    })
+    pkgs.cascadia-code
+    pkgs.jbang
   ];
 
   programs.firefox = {
@@ -24,6 +58,9 @@ in {
     };
   };
   programs.vscode.enable = true;
+
+  # Flag for Wayland support with MS edge
+
 
   programs.git = {
     enable = true;
--- helpers/hidpi.nix
+++ helpers/hidpi.nix
@@ -1,19 +1,13 @@
 # Required tweaks for HiDPI shenanigans
 
-# Add the following to your /etc/nixos/configuration.nix
-
-#  # Enable fractional scaling things
-#  services.xserver.desktopManager.gnome = {
-#    # see https://github.com/NixOS/nixpkgs/issues/114514#issuecomment-1158715877
-#    extraGSettingsOverridePackages = [pkgs.gnome.mutter];
-#    extraGSettingsOverrides = ''
-#      [org.gnome.mutter]
-#      experimental-features=['scale-monitor-framebuffer']
-#    '';
-#  };
-
 { config, pkgs, ... }:
 
 {
+  dconf.settings = {
+    "org/gnome/mutter" = {
+      experimental-features = [ "scale-monitor-framebuffer" ];
+    };
+  };
+
   home.sessionVariables.NIXOS_OZONE_WL = "1"; # https://github.com/NixOS/nixpkgs/commit/b2eb5f62a7fd94ab58acafec9f64e54f97c508a6
 }
